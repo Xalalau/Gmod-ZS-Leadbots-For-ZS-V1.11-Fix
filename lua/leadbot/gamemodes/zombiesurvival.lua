a3e63e0f9533c697c9f24b4fbed1c8a561becdd4
TonyDoskEnginooy <111401791+TonyDoskEnginooy@users.noreply.github.com>
2024-08-17 18:53:57 -0700

    ammo now regens when hinfoammo on

--- lua/leadbot/gamemodes/zombiesurvival.lua
+++ lua/leadbot/gamemodes/zombiesurvival.lua
@@ -2536,8 +2536,8 @@ if SERVER then
                     if maxClip <= 0 and primAmmoType ~= -1 then maxClip = 1 end
                     if maxClip2 == -1 and secAmmoType ~= -1 then maxClip2 = 1 end
                     if n == 1 then
-                        if maxClip > 0 then weapon:SetClip1(maxClip) end
-                        if maxClip2 > 0 then weapon:SetClip2(maxClip2) end
+                        if maxClip >= 0 then weapon:SetClip1(maxClip) end
+                        if maxClip2 >= 0 then weapon:SetClip2(maxClip2) end
                     end
                     if primAmmoType ~= -1 then
                         v:SetAmmo( maxClip, primAmmoType)
